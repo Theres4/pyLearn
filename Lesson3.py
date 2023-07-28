897ea2ee66cbb7f985edb5bf7ae9874053c609a5
Theresa <138995285+Theres4@users.noreply.github.com>
2023-07-28 13:13:44 +0200

    Create Lesson3.py

---
+++ Lesson3.py
@@ -0,0 +1,42 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        raise ValueError("Cannot divide by zero!")
+    return x / y
+
+def calculator():
+    print("Choose an operation:")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+
+    choice = input("Enter the operation number (1/2/3/4): ")
+
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+    if choice == '1':
+        print("Result:", add(num1, num2))
+    elif choice == '2':
+        print("Result:", subtract(num1, num2))
+    elif choice == '3':
+        print("Result:", multiply(num1, num2))
+    elif choice == '4':
+        try:
+            print("Result:", divide(num1, num2))
+        except ValueError as e:
+            print("Error:", e)
+    else:
+        print("Invalid operation choice")
+
+if __name__ == "__main__":
+    calculator()
